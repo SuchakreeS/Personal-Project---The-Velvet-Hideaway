f3f75d2bc517be0d52c705023ab306b8dc399e9b
SuchakreeS <poomfail2013@gmail.com>
2026-05-18 09:25:30 +0700

    feat: initialize express application with routing, CORS configuration, and global error handling

--- src/app.js
+++ src/app.js
@@ -20,11 +20,12 @@ app.use(urlEncodedParser);
 
 app.use(cors({
     origin: [
-        "https://personal-project-the-velvet-hideaway-frontend-ffxcv5y9y.vercel.app", 
-        process.env.FRONTEND_URL, 
-        "http://localhost:5173", 
+        "https://personal-project-the-velvet-hideaway-frontend-qm66hbb8m.vercel.app",
+        "https://personal-project-the-velvet-hideaway-frontend-ffxcv5y9y.vercel.app",
+        process.env.FRONTEND_URL,
+        "http://localhost:5173",
         "http://127.0.0.1:5173"
-    ],
+    ].filter(Boolean),
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }));
